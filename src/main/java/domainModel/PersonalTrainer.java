1c9a77c868622802fc0c5bcdb360ea6f51a4d0c4
TommasoBotarelli <tommaso.botarelli@stud.unifi.it>
2022-03-28 12:02:37 +0200

    Aggiunto metodo equals in personal trainer. Eliminata la classe PersonalTrainerInfo, aggiunti quindi i vari attributi direttamente alla classe personalTrainer.

--- src/main/java/domainModel/PersonalTrainer.java
+++ src/main/java/domainModel/PersonalTrainer.java
@@ -1,13 +1,44 @@
 package domainModel;
 
 public class PersonalTrainer {
-    private EmployeeInfo personalTrainerInfo;
+    private String name;
+    private String surname;
+    private String phoneNumber;
 
-    public PersonalTrainer(String name, String surname, String phoneNumber){
-        this.personalTrainerInfo = new EmployeeInfo(name, surname, phoneNumber);
+    public String getName() {
+        return name;
     }
 
-    public EmployeeInfo getPersonalTrainerInfo() {
-        return personalTrainerInfo;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public PersonalTrainer(String name, String surname, String phoneNumber){
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.name.equalsIgnoreCase(((PersonalTrainer)obj).getName()) &&
+                this.surname.equalsIgnoreCase(((PersonalTrainer)obj).getSurname()) &&
+                this.phoneNumber.equalsIgnoreCase(((PersonalTrainer)obj).getPhoneNumber());
     }
 }
